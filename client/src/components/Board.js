96907baa84aca8566b74cf168f9be94084e6eb56
Fabian Flores <ffflores1@outlook.com>
2019-02-20 22:07:53 -0500

    added grid layout using react-bootstrap

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -4,65 +4,46 @@ import styled from 'styled-components'
 const Table = styled.table`
   background-color: "#f8f5a3";
   color: "#000040";
-  border: 2px solid black;
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+  border-bottom: 2px solid black;
   border-collapse: collapse;
 `;
 
 const TRow = styled.tr`
   background-color: "#f8f5a3";
-  border: 2px solid black;
 `;
 
 const TCell = styled.td`
   background-color: "#f8f5a3";
-  border: 2px solid black;
+  border-right: 2px solid black;
+  border-left: 2px solid black;
 `;
 
 const Board = props => {
   const {playBoard} = props;
 
   return (
-    <div>
-      <div>
-        <h3>Board</h3>
-      </div>
-      <Table>
-      {
-        playBoard.map(function(row, index) {
+    <Table>
+    {
+      playBoard.map(function(row, index) {
 
-          return (
-            <TRow key={index}>
-                {
-                  row.map(function(col, cIndex) {
-                    return (
-                        <TCell key={`{index}{cIndex}`}>Row: {index}, Col: {cIndex}</TCell>
-                    )
-                  })
-                }
-            </TRow>
-          );
+        return (
+          <TRow key={index}>
+              {
+                row.map(function(col, cIndex) {
+                  return (
+                      <TCell key={`{index}{cIndex}`}>Row: {index}, Col: {cIndex}</TCell>
+                  )
+                })
+              }
+          </TRow>
+        );
 
-        })
-      }
-      </Table>
-    </div>
+      })
+    }
+    </Table>
   );
 }
-
-/*
-          return (
-              <tr>
-                  <td>{ub.firstName}</td>
-                  <td>{ub.lastName}</td>
-                  {buttons}
-              </tr>
-          )
-
-body: arr.map( function( row ) {
-    return row.map( function( cell ) {
-        return foo( cell );
-    } );
-} )
-*/
 
 export default Board;
--- client/src/components/ConnectFour.js
+++ client/src/components/ConnectFour.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import Board from './Board';
 import axios from 'axios';
+import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const GRID_ROWS = 7;
 const GRID_COLS = 7;
+
+const Header = styled.header`
+  background-color: #fbea8c;
+  padding: 5px 0;
+`;
+
+const CustomH1 = styled.h1`
+  text-align: center;
+`;
 
 class ConnectFour extends Component {
   constructor(props) {
@@ -23,17 +36,22 @@ class ConnectFour extends Component {
     const {board} = this.state;
 
     return (
-      <div>
-        <header>
-        Connect Four
-        </header>
-        <section>
-          <Board
-            playBoard={board}
-          />
-        </section>
-      </div>
-
+      <Container className="justify-content-sm-center">
+        <Row>
+          <Col xs={12} sm="auto">
+            <Header>
+              <CustomH1>Connect Four</CustomH1>
+            </Header>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={{ span: 6, offset: 3 }}>
+            <Board
+              playBoard={board}
+            />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
