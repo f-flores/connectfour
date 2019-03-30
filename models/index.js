1efe0a2e22f3bc884876a63ec117db8d314e70e5
Fabian Flores <ffflores1@outlook.com>
2019-03-29 22:31:15 -0400

    added db connect function definition in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,33 @@
 // Export All Models
+const mongoose = require('mongoose');
 
 module.exports = {
   GameBoard: require("./GameBoard.js"),
 };
+
+const MONGODB_LOCATION = process.env.MONGODB_URI || "mongodb://localhost/connectfour";
+console.log(`right before connect(io) in ./models/index.js`);
+
+// connect to db
+async function connect(io) {
+  await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/connectfour", { useNewUrlParser: true });
+  console.log('Connected to MongoDB');
+
+  const gameChangeStream = GameBoard.collection.watch({
+    fullDocument: 'updateLookup',
+  });
+  gameChangeStream.on('change', event => {
+    console.log('it changed', event);
+    /*
+    io.emit('dib changeEvent', {
+      type: result.operationType,
+      dib: {
+        claimed: {},
+        ...result.fullDocument,
+        id: result.fullDocument._id,
+      },
+    });
+    */
+  });
+}
+exports.connect = connect;
